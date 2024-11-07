cf481ab2ab14b487bf8ebd21780afcbc045037c9
Nazwa8 <144130807+Nazwa8@users.noreply.github.com>
2024-11-07 11:17:28 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/zadanie2/MainActivity.java
+++ app/src/main/java/com/example/zadanie2/MainActivity.java
@@ -7,7 +7,6 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import kotlin.collections.IntIterator;
 
 //testowanie commit 2.0.0.0.0.0
 
